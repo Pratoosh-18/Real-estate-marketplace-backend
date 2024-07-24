50c53ecb22e1070a376df72ed43a5d5177eff0f3
Pratoosh-18 <pratoosh10garg@gmail.com>
2024-07-24 23:35:38 +0530

    fixed apiError error

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 import { User } from "../models/User.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError } from "../utils/ApiError.js";
+import { ApiError } from "../utils/apiError.js";
 
 export const verifyJWT = asyncHandler(async (req, _, next) => {
     try {
